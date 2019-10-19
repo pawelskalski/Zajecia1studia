729cf69c2cc0649574a1e8c24d46be3c3af3dad3
Paweł Skalski <40362366+pawelskalski@users.noreply.github.com>
2019-10-19 13:45:04 +0200

    Uzupełnienie

---
+++ Pierwszezajecia/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pierwszezajecia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
+        }
+    }
+}
